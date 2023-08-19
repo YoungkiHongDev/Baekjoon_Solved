edbadacd1603b360eb106365e6318b4fbce12399
YoungkiHongDev <dudrl1410@naver.com>
2023-08-19 22:19:02 +0900

    백준 19532 - 수학은 비대면강의입니다

--- Bronze/bj19532.py
+++ Bronze/bj19532.py
@@ -6,6 +6,7 @@ a, b, c, d, e, f = map(int, input().split())
 
 for x in range(-999, 1000):
     for y in range(-999, 1000):
+        # 연립방정식 조건에 맞는 답을 찾으면 출력한다.
         if (a*x) + (b*y) == c and (d*x) + (e*y) == f:
             print(x, y)
             break
